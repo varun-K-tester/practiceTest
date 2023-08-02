e2d106332f16e7148f2589ad4c22124a56c3f527
2118813 <2118813@LTIN298081.cts.com>
2023-08-02 11:53:19 +0530

    Cts_changes

--- Scripts/HandsOn/Runner/Script1689312577722.groovy
+++ Scripts/HandsOn/Runner/Script1689312577722.groovy
@@ -17,9 +17,9 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-CucumberKW.runFeatureFile('Include/features/HandsOn/Open_Browser.feature')
-CucumberKW.runFeatureFile('Include/features/HandsOn/login.feature')
-CucumberKW.runFeatureFile('Include/features/HandsOn/ChooseProduct.feature')
+//CucumberKW.runFeatureFile('Include/features/HandsOn/Open_Browser.feature')
+//CucumberKW.runFeatureFile('Include/features/HandsOn/login.feature')
+//CucumberKW.runFeatureFile('Include/features/HandsOn/ChooseProduct.feature')
 CucumberKW.runFeatureFile('Include/features/HandsOn/AddToCart.feature')
 CucumberKW.runFeatureFile('Include/features/HandsOn/PlaceOrder.feature')
 CucumberKW.runFeatureFile('Include/features/HandsOn/Close_site.feature')
